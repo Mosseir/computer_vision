d8f0cb7f5779e2463f403f2ef925588e3feca4be
Mozzoer <chayakorn.siri@hotmail.com>
2024-11-28 10:37:21 +0700

    feat: commit lenlen

--- example1_2_resize_flip_filter.py
+++ example1_2_resize_flip_filter.py
@@ -12,26 +12,27 @@ while(True):
     ############ Resizing ############################
     TARGET_SIZE = (360,240)
     im_resized = cv2.resize(im,TARGET_SIZE)
-    print(im_resized.shape)
+    # print(im_resized.shape)
     ##################################################
 
     ############ Flipping ############################
-    #im_flipped = cv2.flip(im_resized,1)
+    im_flipped = cv2.flip(im_resized,1)
     ##################################################
 
     ############ Blurred ############################
-    #L = 25
-    #kernel = np.ones((L, L), np.float32) / L / L
-    #im_blurred = cv2.filter2D(im_flipped, -1, kernel)
+    L = 25 ##ยิ่งเยอะยิ่งเบลอ (moving avg.)
+    kernel = np.ones((L, L), np.float32) / L / L
+    im_blurred = cv2.filter2D(im_flipped, -1, kernel)
     ##################################################
     
     ############ Median Filter #######################
-    #L = 25
-    #im_median = cv2.medianBlur(im_flipped, L)
+    L = 25
+    im_median = cv2.medianBlur(im_flipped, L)
     ##################################################
 
     cv2.imshow('original', im)
-    cv2.imshow('modified', im_resized) # Change variable name for displaying another images 
+    cv2.imshow('modified', im_blurred) # Change variable name for displaying another images 
+    cv2.imshow('median', im_median)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- example1_3a_blending.py
+++ example1_3a_blending.py
@@ -9,16 +9,18 @@ im_flipped = cv2.flip(im_resized, 1)
 im0 = im1 = im2 = im3 = im_flipped
 
 while(True):
-    im0 = im1
-    im1 = im2
-    im2 = im3
-    im3 = im_flipped
+    im0 = im1 #im0 = t - 4
+    im1 = im2 #im1 =  t - 3
+    im2 = im3 #im2 = t - 2
+    im3 = im_flipped #im3 = t - 1 , im_flipped = t
     
     ret,im = cap.read()
     im_resized = cv2.resize(im, (640,360))
     im_flipped = cv2.flip(im_resized, 1)
 
-    im_out = (0.2*im0 + 0.2*im1 + 0.2*im2 + 0.2*im3 + 0.2*im_flipped).astype(np.uint8)
+    # im_out = (0.2*im0 + 0.2*im1 + 0.2*im2 + 0.2*im3 + 0.2*im_flipped).astype(np.uint8) #no astype(np.uint8) รันได้แต่ไม่ตรงตามต้องการแน่ๆ
+    # im_out = ((im0 + im1 + im2 + im3 + im_flipped) / 5).astype(np.uint8) #Overflow example
+    im_out = (im_flipped*2).astype(np.uint8) #Overflow example
     cv2.imshow('camera',im_out)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
